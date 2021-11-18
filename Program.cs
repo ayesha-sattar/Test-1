e15c9ba7b8384e0d3d30979e1588cdd9d7f34c67
ayesha-sattar <aishachaudhary816@gmail.com>
2021-11-18 19:39:54 +0500

    First commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Test_1
     {
         static void Main(string[] args)
         {
-            int a=5 , b=6 , c=4;
+          /*  int a=5 , b=6 , c=4;
             Console.WriteLine(--a*b - ++c);
 
             Console.WriteLine("Hello World!");
@@ -14,9 +14,23 @@ namespace Test_1
             Console.WriteLine(-1+4*6);
             Console.WriteLine((35+5)%7);
             Console.WriteLine(14+-4*6/11);
-            Console.WriteLine(2+15/6*1-7%2);
+            Console.WriteLine(2+15/6*1-7%2);*/
+
+            Console.WriteLine("result/=2;");
+
+            Console.WriteLine("x=x+2;");
+
+           Console.WriteLine("const float PI = 3.142;"); 
+
+           Console.WriteLine("int i = 10;");
+
+           Console.WriteLine("int start=0 , end=0;");
+
+
 
             
+
+
         }
     }
 }
