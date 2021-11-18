e63e157d45a7578fc1fe100e5a5d7efd029031aa
ayesha-sattar <aishachaudhary@gmail.com>
2021-11-18 19:00:56 +0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=5 , b=6 , c=4;
+            Console.WriteLine(--a*b - ++c);
+
+            Console.WriteLine("Hello World!");
+ 
+            Console.WriteLine(-1+4*6);
+            Console.WriteLine((35+5)%7);
+            Console.WriteLine(14+-4*6/11);
+            Console.WriteLine(2+15/6*1-7%2);
+
+            
+        }
+    }
+}
